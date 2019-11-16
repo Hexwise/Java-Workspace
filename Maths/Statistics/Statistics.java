0026ac0555106cd50833b8d063df71a7a476e2cd
ehunt <ehunt@uno.edu>
2019-11-16 00:50:03 -0600

    Add .gitignore, Update Statistics.java

--- Maths/Statistics/Statistics.java
+++ Maths/Statistics/Statistics.java
@@ -9,13 +9,74 @@ public class Statistics {
        Scanner sc = new Scanner(System.in);
        int n = Integer.parseInt(sc.nextLine().trim());
        int[] array = new int[n];
+       double[] d = new double[n];
        for(int i=0; i<n; i++) {
            array[i] = sc.nextInt();
+           d[i] = (double) array[i];
        }
-       System.out.println(mean(array, n));
-       System.out.println(median(array, n));
-       System.out.println(mode(array, n));
+       System.out.println("mean: " + mean(array));
+       System.out.println("median: " + median(array));
+       System.out.println("mode: " + mode(array, n));
+       System.out.println("pop. variance.: " + popVariance(d));
+       System.out.println("pop. std. dev.: " + popStdDev(d));
+       System.out.println("sample variance.: " + sampleVariance(d));
+       System.out.println("sample std. dev.: " + sampleStdDev(d));
        sc.close();
+    }
+
+    public static double popVariance(double a[]) { 
+        // Compute mean (average  
+        // of elements) 
+        double n = a.length;
+        double sum = 0; 
+        for (int i=0; i<(int)n; i++) 
+            sum += a[i]; 
+        double mean = sum / n; 
+      
+        // Compute sum squared  
+        // differences with mean. 
+        double sqDiff = 0; 
+        for (int i = 0; i < n; i++)  
+            sqDiff += (a[i] - mean) * (a[i] - mean); 
+          
+        return sqDiff / n; 
+    } 
+      
+    public static double popStdDev(double arr[]) { 
+        return Math.sqrt(popVariance(arr)); 
+    }
+
+    public static double sampleVariance(double a[]) { 
+        // Compute mean (average  
+        // of elements) 
+        double n = a.length;
+        double sum = 0; 
+        for (int i=0; i<(int)n; i++) 
+            sum += a[i]; 
+        double mean = sum / n; 
+      
+        // Compute sum squared  
+        // differences with mean. 
+        double sqDiff = 0; 
+        for (int i = 0; i < n; i++)  
+            sqDiff += (a[i] - mean) * (a[i] - mean); 
+          
+        return sqDiff / (n-1); 
+    } 
+      
+    public static double sampleStdDev(double arr[]) { 
+        return Math.sqrt(sampleVariance(arr)); 
+    } 
+
+    public static double CompstdDev(int[] array) { // not working
+        int sum = 0;
+        int sqrSum = 0;
+        int n = array.length;
+        for(int i=0; i<n; i++) {
+            sum += array[i];
+            sqrSum += (int) Math.pow(array[i], 2);
+        }
+        return Math.sqrt((sqrSum - Math.pow(sum, 2) / n) / n - 1); 
     }
 
     public static double mean(int[] array) {
@@ -23,9 +84,9 @@ public class Statistics {
         for(int i=0; i<array.length; i++) {
             sum += array[i];
         }
-        return (double)(sum)/ (double)(array.length-1);
+        return (double)(sum)/ (double)(array.length);
     }
-
+    
     public static double meanRecursive(int[] array, int n) {
         if (n == 1)
             return (double) array[n-1]; 
@@ -43,6 +104,15 @@ public class Statistics {
         return (double)(sum)/ (double)(weightSum);
     }
 
+    public static double median(int[] array) {
+        Arrays.sort(array);
+        int n = array.length;
+        if(n%2==0) {
+            return ((double) array[n/2-1] + (double) array[n/2]) / 2;
+        }
+        return (double) array[n/2];
+    }
+
     public static double median(int[] array, int n) {
         Arrays.sort(array);
         if(n%2==0) {
@@ -51,7 +121,61 @@ public class Statistics {
         return (double) array[n/2];
     }
 
+    public static int mode(int[] array) {
+        // Sort the array 
+        Arrays.sort(array);
+        int n = array.length;
+        // Find the max frequency using linear traversal 
+        int max_count = 1, res = array[0], curr_count = 1; 
+        for (int i = 1; i < n; i++) { 
+            if (array[i] == array[i - 1]) 
+                curr_count++; 
+            else { 
+                if (curr_count > max_count) { 
+                    max_count = curr_count; 
+                    res = array[i - 1]; 
+                } 
+                curr_count = 1; 
+            } 
+        } 
+    
+        // If last element is most frequent 
+        if (curr_count > max_count) 
+        { 
+            max_count = curr_count; 
+            res = array[n - 1]; 
+        } 
+        return res; 
+    }
+    
     public static int mode(int[] array, int n) {
+        // Sort the array 
+        Arrays.sort(array);
+    
+        // Find the max frequency using linear traversal 
+        int max_count = 1, res = array[0], curr_count = 1; 
+        for (int i = 1; i < n; i++) { 
+            if (array[i] == array[i - 1]) 
+                curr_count++; 
+            else { 
+                if (curr_count > max_count) { 
+                    max_count = curr_count; 
+                    res = array[i - 1]; 
+                } 
+                curr_count = 1; 
+            } 
+        } 
+    
+        // If last element is most frequent 
+        if (curr_count > max_count) 
+        { 
+            max_count = curr_count; 
+            res = array[n - 1]; 
+        } 
+        return res; 
+    }
+
+    public static int mode2(int[] array, int n) {
         // Insert all elements in hash 
         Map<Integer, Integer> hm = new HashMap<Integer, Integer>(); 
           
@@ -77,32 +201,5 @@ public class Statistics {
         } 
         return res; 
     }
-
-    public static int mode2(int[] array, int n) {
-        // Sort the array 
-        Arrays.sort(array);
-    
-        // Find the max frequency using linear traversal 
-        int max_count = 1, res = array[0], curr_count = 1; 
-        for (int i = 1; i < n; i++) { 
-            if (array[i] == array[i - 1]) 
-                curr_count++; 
-            else { 
-                if (curr_count > max_count) { 
-                    max_count = curr_count; 
-                    res = array[i - 1]; 
-                } 
-                curr_count = 1; 
-            } 
-        } 
-    
-        // If last element is most frequent 
-        if (curr_count > max_count) 
-        { 
-            max_count = curr_count; 
-            res = array[n - 1]; 
-        } 
-        return res; 
-    } 
 }
 
