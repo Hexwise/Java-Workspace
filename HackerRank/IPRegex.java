9f8932a4ae567e016d8d41903c886653b27d4f1a
Hexwise <ghola.eyes@gmial.com>
2020-10-28 13:04:35 -0500

    Update Java-Workspace Repository.

---
+++ HackerRank/IPRegex.java
@@ -0,0 +1,4 @@
+final class IPRegex {
+    String zeroTo255 = "(\\d{1, 2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+    String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+}
---
+++ HackerRank/SplitString.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class SplitString {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.trim();
+        if(s.isEmpty()) {
+            System.out.println("0");
+            scan.close();
+            return;
+        }
+        String[] substrings = s.split("[^A-Za-z]+");
+        System.out.println(substrings.length);
+        for(int i=0; i<substrings.length; i++) {
+            System.out.println(substrings[i]);
+        }
+        scan.close();
+    }
+}
